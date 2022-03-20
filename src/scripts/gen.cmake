640c400f902d7e575f4fd9b818b4046a125e6433
p0lyh3dron <thep0lyh3dron@gmail.com>
2022-03-20 14:47:58 -0400

    general: add initial project setup

---
+++ src/scripts/gen.cmake
@@ -0,0 +1,3 @@
+set_property( GLOBAL PROPERTY CHIK       ${CMAKE_CURRENT_LIST_DIR} )
+set_property( GLOBAL PROPERTY LIBCHIK    ${CMAKE_CURRENT_LIST_DIR}/../libchik )
+set_property( GLOBAL PROPERTY CHIK_GAMES ${CMAKE_CURRENT_LIST_DIR}/../games )
