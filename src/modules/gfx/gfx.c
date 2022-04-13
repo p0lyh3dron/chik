e9474fecea34cb4d76422fe0598d85f04b2791b7
p0lyh3dron <thep0lyh3dron@gmail.com>
2022-04-12 21:14:40 -0400

    gfx: vertex attribute interpolation

--- src/modules/gfx/gfx.c
+++ src/modules/gfx/gfx.c
@@ -11,6 +11,8 @@
  */
 #include "libchik.h"
 
+#include <string.h>
+
 #include "abstract.h"
 #include "rendertarget.h"
 
@@ -20,137 +22,151 @@ resource_t *gpVBuffer = NULL;
 resource_t *gpGResources = NULL;
 
 /*
- *    Calculate the color differential.
+ *    Draws vertex color to the fragment.
  *
- *    @param color32_t *    The first color.
- *    @param color32_t *    The second color.
- * 
- *    @param f32            The divisor.
- * 
- *    @return vec4_t        The incremental color.
+ *    @param fragment_t *    The fragment to draw to.
+ *    @param vec_t *         The color to draw.
  */
-vec4_t color_diff( color32_t *spColor1, color32_t *spColor2, f32 sDivisor ) {
-    vec4_t sColor;
-    
-    sColor.x = ( spColor1->r - spColor2->r ) / sDivisor;
-    sColor.y = ( spColor1->g - spColor2->g ) / sDivisor;
-    sColor.z = ( spColor1->b - spColor2->b ) / sDivisor;
-    sColor.w = ( spColor1->a - spColor2->a ) / sDivisor;
-    
-    return sColor;
+void draw_vertex_color( fragment_t *spFrag, vec_t *spColor ) {
+    spFrag->aColor.r = spColor->v4.x * 255.0f;
+    spFrag->aColor.g = spColor->v4.y * 255.0f;
+    spFrag->aColor.b = spColor->v4.z * 255.0f;
+    spFrag->aColor.a = spColor->v4.w * 255.0f;
 }
 
-/*
- *    Calculate an arbitrary color between two colors.
- *
- *    @param color32_t *    The final color.
- *    @param f32            The variable.
- *    @param vec4_t *    The differential.
- * 
- *    @return color32_t     The calculated color.
- */
-color32_t color_lerp( color32_t *spColor, f32 sVariable, vec4_t *spColorDiff ) {
-    color32_t sColor;
-    
-    sColor.r = spColor->r + ( spColorDiff->x ) * ( spColor->r - sVariable );
-    sColor.g = spColor->g + ( spColorDiff->y ) * ( spColor->g - sVariable );
-    sColor.b = spColor->b + ( spColorDiff->z ) * ( spColor->b - sVariable );
-    sColor.a = spColor->a + ( spColorDiff->w ) * ( spColor->a - sVariable );
-    
-    return sColor;
-}
+v_layout_t gVLayout = {
+    .aAttribs = { 
+        { V_POS, V_R32G32B32_F,     sizeof( vec3_t ), 0,  NULL               },
+        { 0,     V_R32G32B32A32_F,  sizeof( vec4_t ), 12, &draw_vertex_color },
+    },
+    .aCount   = 2,
+};
 
 /*
  *    Draw a scanline.
  *
- *    @param chik_vertex_t       The starting x position of the scanline.
- *    @param chik_vertex_t       The ending x position of the scanline.
- *    @param int                 The starting y position of the scanline.
+ *    @param s32                  The initial x coordinate.
+ *    @param s32                  The final x coordinate.
+ *    @param vec_t *              A pointer to initial interpolated vector data.
+ *    @param vec_t *              A pointer to the final interpolated vector data.
+ *    @param int                  The starting y position of the scanline.
  */
-void draw_scanline( chik_vertex_t x1, chik_vertex_t x2, int y ) {
+void draw_scanline( s32 x0, s32 x1, vec_t *v0, vec_t *v1, int y ) {
     /*
      *    Early out if the scanline is outside the render target,
      *    or if the line is a degenerate.
      */
-    if ( y < 0 || y >= gpBackBuffer->apTarget->aHeight || x1.aRastPos.x == x2.aRastPos.x ) {
+    if ( y < 0 || y >= gpBackBuffer->apTarget->aHeight || ( x0 < 0 && x1 < 0 ) ) {
         return;
     }
-    /*
-     *    Rasterize the scanline.
-     */
-    chik_vertex_t x = x1;
-                  x.aRastPos.x = MIN( x1.aRastPos.x, x2.aRastPos.x );
-                  x.aRastPos.x = MAX( x.aRastPos.x, 0 );
+    s32 x = MIN( x0, x1 );
+        x = MAX( x, 0 );
 
-    chik_vertex_t xEnd = x2;
-                  xEnd.aRastPos.x = MAX( x1.aRastPos.x, x2.aRastPos.x );
-                  xEnd.aRastPos.x = MIN( xEnd.aRastPos.x, ( s32 )gpBackBuffer->apTarget->aWidth );
+    s32 endX = MAX( x0, x1 );
+        endX = MIN( endX, gpBackBuffer->apTarget->aWidth );
 
-    /*
-     *    Interpolate color.
-     */
-    float dr = ( float )( xEnd.aColor.r - x.aColor.r ) / ( xEnd.aRastPos.x - x.aRastPos.x );
-    float dg = ( float )( xEnd.aColor.g - x.aColor.g ) / ( xEnd.aRastPos.x - x.aRastPos.x );
-    float db = ( float )( xEnd.aColor.b - x.aColor.b ) / ( xEnd.aRastPos.x - x.aRastPos.x );
-    float da = ( float )( xEnd.aColor.a - x.aColor.a ) / ( xEnd.aRastPos.x - x.aRastPos.x );
-
-    color32_t color = { 0 };
-
-    while ( x.aRastPos.x < xEnd.aRastPos.x ) {
+    vec_t v[ MAX_VECTOR_ATTRIBUTES ];
+    while ( x < endX ) {
+        fragment_t f = {
+            .aPos = { x, y },
+        };
         /*
-         *    Don't draw outside the render target.
+         *    Interpolate the vector values, and apply to the fragment.
          */
-        if ( x.aRastPos.x >= 0 && x.aRastPos.x < gpBackBuffer->apTarget->aWidth && y >= 0 && y < gpBackBuffer->apTarget->aHeight ) {
-            /*
-             *    Color the pixel.
-             */
-            color.r = ( xEnd.aRastPos.x - x.aRastPos.x ) * dr + xEnd.aColor.r;
-            color.g = ( xEnd.aRastPos.x - x.aRastPos.x ) * dg + xEnd.aColor.g;
-            color.b = ( xEnd.aRastPos.x - x.aRastPos.x ) * db + xEnd.aColor.b;
-            color.a = ( xEnd.aRastPos.x - x.aRastPos.x ) * da + xEnd.aColor.a;
+        for ( u32 i = 0; i < gVLayout.aCount; i++ ) {
+            v[ i ] = vec_interp( ( void* )&v0[ i ], ( void* )&v1[ i ], ( f32 )( x - x0 ) / ( endX - x0 ), gVLayout.aAttribs[ i ].aFormat );
 
-            gpBackBuffer->apTarget->apData[ x.aRastPos.x + y * gpBackBuffer->apTarget->aWidth     ] = *( u32* )&color;
+            if ( gVLayout.aAttribs[ i ].apFunc ) {
+                gVLayout.aAttribs[ i ].apFunc( &f, &v[ i ] );
+            }
         }
-        x.aRastPos.x++;
+
+        /*
+         *    Draw the vertex.
+         */
+        memcpy( gpBackBuffer->apTarget->apData + ( y * gpBackBuffer->apTarget->aWidth + x ), &f.aColor, sizeof( f.aColor ) );
+
+        x++;
     }
 }
 
 /*
  *    Draws a triangle.
  *
- *    @param vertex_t    The first point of the triangle.
- *    @param vertex_t    The second point of the triangle.
- *    @param vertex_t    The third point of the triangle.
+ *    @param void *     The data for the first point of the triangle.
+ *    @param void *     The data for the second point of the triangle.
+ *    @param void *     The data for the third point of the triangle.
  */
-void draw_triangle( chik_vertex_t a, chik_vertex_t b, chik_vertex_t c ) {
+void draw_triangle( void *pA, void *pB, void *pC ) {
+    /*
+     *    Extract the data from the vertices.
+     */
+    u32 offset = 0;
+    for ( u32 i = 0; i < gVLayout.aCount; i++ ) {
+        if ( gVLayout.aAttribs[ i ].aUsage & V_POS ) {
+            break;
+        }
+        else {
+            offset += get_vertex_component_size( gVLayout.aAttribs[ i ].aFormat );
+        }
+    }
+
+    /*
+     *    Extract position data, and rasterize it.
+     */
+    int y0 = ( *( float* )( pA + offset + sizeof( float ) * 1 ) + 1 ) * gpBackBuffer->apTarget->aHeight / 2;
+    int y1 = ( *( float* )( pB + offset + sizeof( float ) * 1 ) + 1 ) * gpBackBuffer->apTarget->aHeight / 2;
+    int y2 = ( *( float* )( pC + offset + sizeof( float ) * 1 ) + 1 ) * gpBackBuffer->apTarget->aHeight / 2;
+
+    int x0 = ( *( float* )( pA + offset + sizeof( float ) * 0 ) + 1 ) * gpBackBuffer->apTarget->aWidth / 2;
+    int x1 = ( *( float* )( pB + offset + sizeof( float ) * 0 ) + 1 ) * gpBackBuffer->apTarget->aWidth / 2;
+    int x2 = ( *( float* )( pC + offset + sizeof( float ) * 0 ) + 1 ) * gpBackBuffer->apTarget->aWidth / 2;
+
+
     /*
      *    Sort the vertices by y-coordinate.
      *
-     *    a at the top, b at the middle, c at the bottom.
+     *    y0 at the top, y1 at the middle, y2 at the bottom.
      */
-    if ( a.aPos.y < b.aPos.y ) {
-        chik_vertex_t temp = a;
-        a = b;
-        b = temp;
-    }
-    if ( b.aPos.y < c.aPos.y ) {
-        chik_vertex_t temp = b;
-        b = c;
-        c = temp;
-    }
-    if ( a.aPos.y < b.aPos.y ) {
-        chik_vertex_t temp = a;
-        a = b;
-        b = temp;
-    }
+    if ( y0 < y1 ) {
+        void *pTemp = pA;
+        pA = pB;
+        pB = pTemp;
 
-    int y0 = ( a.aPos.y + 1 ) * gpBackBuffer->apTarget->aHeight / 2;
-    int y1 = ( b.aPos.y + 1 ) * gpBackBuffer->apTarget->aHeight / 2;
-    int y2 = ( c.aPos.y + 1 ) * gpBackBuffer->apTarget->aHeight / 2;
+        int temp = y0;
+        y0 = y1;
+        y1 = temp;
 
-    int x0 = ( a.aPos.x + 1 ) * gpBackBuffer->apTarget->aWidth / 2;
-    int x1 = ( b.aPos.x + 1 ) * gpBackBuffer->apTarget->aWidth / 2;
-    int x2 = ( c.aPos.x + 1 ) * gpBackBuffer->apTarget->aWidth / 2;
+        temp = x0;
+        x0 = x1;
+        x1 = temp;
+    }
+    if ( y1 < y2 ) {
+        void *pTemp = pB;
+        pB = pC;
+        pC = pTemp;
+
+        int temp = y1;
+        y1 = y2;
+        y2 = temp;
+
+        temp = x1;
+        x1 = x2;
+        x2 = temp;
+    }
+    if ( y0 < y1 ) {
+        void *pTemp = pA;
+        pA = pB;
+        pB = pTemp;
+
+        int temp = y0;
+        y0 = y1;
+        y1 = temp;
+
+        temp = x0;
+        x0 = x1;
+        x1 = temp;
+    }
 
     /*
      *    If the triangle is flat, return.
@@ -160,41 +176,60 @@ void draw_triangle( chik_vertex_t a, chik_vertex_t b, chik_vertex_t c ) {
     }
 
     /*
+     *    Rasterize the starting y position.
+     */
+    int y = MAX( y0, 0 );
+        y = MIN( y, gpBackBuffer->apTarget->aHeight );
+
+    /*
      *    Calculate the slopes of the lines.
      */
     float dy0 = ( ( float )x1 - x0 ) / ( y1 - y0 );
     float dy1 = ( ( float )x2 - x0 ) / ( y2 - y0 );
     float dy2 = ( ( float )x2 - x1 ) / ( y2 - y1 );
 
-    /*
-     *    Calculate the slopes of the colors.
-     */
-    vec4_t dcolor0 = color_diff( &a.aColor, &b.aColor, y1 - y0 );
-    vec4_t dcolor1 = color_diff( &a.aColor, &c.aColor, y2 - y0 );
-    vec4_t dcolor2 = color_diff( &b.aColor, &c.aColor, y2 - y1 );
-    
-    /*
-     *    Rasterize the starting y position.
-     */
-    int y = MAX( y0, 0 );
-        y = MIN( y, gpBackBuffer->apTarget->aHeight );
+    y2 = MAX( y2, 0 );
 
-    y2    = MIN( y2, gpBackBuffer->apTarget->aHeight );
-    y1    = MIN( y1, gpBackBuffer->apTarget->aHeight );
+    vec_t v0[ MAX_VECTOR_ATTRIBUTES ];
+    vec_t v [ MAX_VECTOR_ATTRIBUTES ];
 
     /*
      *    Check for flat top.
      */
     if ( y0 == y1 ) {
-        while ( y >= y2 ) {
-            chik_vertex_t v0 = a;
-            v0.aRastPos.x = x0 + ( y - y0 ) * dy1;
-            v0.aColor     = color_lerp( &b.aColor, y, &dcolor1 );
-            chik_vertex_t v  = b;
-            v.aRastPos.x  = x1 + ( y - y0 ) * dy2;
-            v.aColor      = color_lerp( &c.aColor, y, &dcolor2 );
+        /*
+         *    Sort top two vertices by x-coordinate, 
+         *    y0 at the left, y1 at the right.
+         */
+        if ( x0 < x1 ) {
+            void *pTemp = pA;
+            pA = pB;
+            pB = pTemp;
 
-            draw_scanline( v0, v, y );
+            int temp = y0;
+            y0 = y1;
+            y1 = temp;
+
+            temp = x0;
+            x0 = x1;
+            x1 = temp;
+        }
+        while ( y >= y2 ) {
+            for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                v0[ i ] = vec_interp( 
+                    pB + gVLayout.aAttribs[ i ].aOffset, 
+                    pC + gVLayout.aAttribs[ i ].aOffset, 
+                    ( f32 )( y0 - y ) / ( y0 - y2 ), 
+                    gVLayout.aAttribs[ i ].aFormat
+                );
+                v[ i ] = vec_interp( 
+                    pA + gVLayout.aAttribs[ i ].aOffset, 
+                    pC + gVLayout.aAttribs[ i ].aOffset, 
+                    ( f32 )( y0 - y ) / ( y0 - y2 ), 
+                    gVLayout.aAttribs[ i ].aFormat
+                );
+            }
+            draw_scanline( x1 + ( y - y0 ) * dy2, x0 + ( y - y0 ) * dy1, v0, v, y );
             y--;
         }
         return;
@@ -204,15 +239,39 @@ void draw_triangle( chik_vertex_t a, chik_vertex_t b, chik_vertex_t c ) {
      *    Check for flat bottom.
      */
     else if ( y1 == y2 ) {
-        while ( y >= y2 ) {
-            chik_vertex_t v0 = a;
-            v0.aRastPos.x = x0 + ( y - y0 ) * dy0;
-            v0.aColor     = color_lerp( &b.aColor, y, &dcolor0 );
-            chik_vertex_t v  = b;
-            v.aRastPos.x  = x0 + ( y - y0 ) * dy1;
-            v.aColor      = color_lerp( &c.aColor, y, &dcolor1 );
+        /*
+         *    Sort bottom two vertices by x-coordinate, 
+         *    y1 at the left, y2 at the right.
+         */
+        if ( x1 < x2 ) {
+            void *pTemp = pB;
+            pB = pC;
+            pC = pTemp;
 
-            draw_scanline( v0, v, y );
+            int temp = y1;
+            y1 = y2;
+            y2 = temp;
+
+            temp = x1;
+            x1 = x2;
+            x2 = temp;
+        }
+        while ( y >= y2 ) {
+            for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                v0[ i ] = vec_interp( 
+                    pA + gVLayout.aAttribs[ i ].aOffset, 
+                    pC + gVLayout.aAttribs[ i ].aOffset, 
+                    ( f32 )( y0 - y ) / ( y0 - y2 ), 
+                    gVLayout.aAttribs[ i ].aFormat
+                );
+                v[ i ] = vec_interp( 
+                    pA + gVLayout.aAttribs[ i ].aOffset, 
+                    pB + gVLayout.aAttribs[ i ].aOffset, 
+                    ( f32 )( y0 - y ) / ( y0 - y2 ), 
+                    gVLayout.aAttribs[ i ].aFormat
+                );
+            }
+            draw_scanline( x0 + ( y - y0 ) * dy0, x0 + ( y - y0 ) * dy1, v0, v, y );
             y--;
         }
         return;
@@ -223,38 +282,70 @@ void draw_triangle( chik_vertex_t a, chik_vertex_t b, chik_vertex_t c ) {
          *    Bend is on the left.
          */
         if ( x1 < x2 ) {
-            chik_vertex_t v0 = a;
-            chik_vertex_t v  = b;
-            v.aRastPos.x  = x0 + ( y - y0 ) * dy1;
-            v.aColor      = color_lerp( &c.aColor, y, &dcolor1 );
+            for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                v[ i ] = vec_interp( 
+                    pA + gVLayout.aAttribs[ i ].aOffset, 
+                    pC + gVLayout.aAttribs[ i ].aOffset, 
+                    ( f32 )( y0 - y ) / ( y0 - y2 ), 
+                    gVLayout.aAttribs[ i ].aFormat
+                );
+            }
             if ( y >= y1 ) {
-                v0.aRastPos.x  = x0 + ( y - y0 ) * dy0;
-                v0.aColor      = color_lerp( &b.aColor, y, &dcolor0 );
-                draw_scanline( v0, v, y );
+                for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                    v0[ i ] = vec_interp( 
+                        pA + gVLayout.aAttribs[ i ].aOffset, 
+                        pB + gVLayout.aAttribs[ i ].aOffset, 
+                        ( f32 )( y0 - y ) / ( y0 - y1 ), 
+                        gVLayout.aAttribs[ i ].aFormat
+                    );
+                }
+                draw_scanline( x0 + ( y - y0 ) * dy0, x0 + ( y - y0 ) * dy1, v0, v, y );
             }
             else {
-                v0.aRastPos.x = x1 + ( y - y1 ) * dy2;
-                v0.aColor     = color_lerp( &c.aColor, y, &dcolor2 );
-                draw_scanline( v0, v, y );
+                for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                    v0[ i ] = vec_interp( 
+                        pB + gVLayout.aAttribs[ i ].aOffset, 
+                        pC + gVLayout.aAttribs[ i ].aOffset, 
+                        ( f32 )( y1 - y ) / ( y1 - y2 ), 
+                        gVLayout.aAttribs[ i ].aFormat
+                    );
+                }
+                draw_scanline( x1 + ( y - y1 ) * dy2, x0 + ( y - y0 ) * dy1, v0, v, y );
             }
         }
         /*
          *    Bend is on the right.
          */
         else {
-            chik_vertex_t v0 = a;
-            v0.aRastPos.x = x0 + ( y - y0 ) * dy1;
-            v0.aColor     = color_lerp( &b.aColor, y, &dcolor1 );
-            chik_vertex_t v  = b;
+            for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                v0[ i ] = vec_interp( 
+                    pA + gVLayout.aAttribs[ i ].aOffset, 
+                    pC + gVLayout.aAttribs[ i ].aOffset, 
+                    ( f32 )( y0 - y ) / ( y0 - y2 ), 
+                    gVLayout.aAttribs[ i ].aFormat
+                );
+            }
             if ( y >= y1 ) {
-                v.aRastPos.x = x0 + ( y - y0 ) * dy0;
-                v.aColor     = color_lerp( &b.aColor, y, &dcolor0 );
-                draw_scanline( v0, v, y );
+                for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                    v[ i ] = vec_interp( 
+                        pA + gVLayout.aAttribs[ i ].aOffset, 
+                        pB + gVLayout.aAttribs[ i ].aOffset, 
+                        ( f32 )( y0 - y ) / ( y0 - y1 ), 
+                        gVLayout.aAttribs[ i ].aFormat
+                    );
+                }
+                draw_scanline( x0 + ( y - y0 ) * dy1, x0 + ( y - y0 ) * dy0, v0, v, y );
             }
             else {
-                v.aRastPos.x = x1 + ( y - y1 ) * dy2;
-                v.aColor     = color_lerp( &c.aColor, y, &dcolor2 );
-                draw_scanline( v0, v, y );
+                for ( u32 i = 0; i < gVLayout.aCount; ++i ) {
+                    v[ i ] = vec_interp( 
+                        pB + gVLayout.aAttribs[ i ].aOffset, 
+                        pC + gVLayout.aAttribs[ i ].aOffset, 
+                        ( f32 )( y1 - y ) / ( y1 - y2 ), 
+                        gVLayout.aAttribs[ i ].aFormat
+                    );
+                }
+                draw_scanline( x0 + ( y - y0 ) * dy1, x1 + ( y - y1 ) * dy2, v0, v, y );
             }
         }
         y--;
@@ -270,7 +361,8 @@ void draw_triangle( chik_vertex_t a, chik_vertex_t b, chik_vertex_t c ) {
  *    @return handle_t          The handle to the vertex buffer.
  */
 handle_t create_vertex_buffer( chik_vertex_t *spVertices, u32 sCount ) {
-    gpVBuffer       = resource_new( 1024 * 1024 * sizeof( chik_vertex_t ) );
+    if ( !gpVBuffer )
+        gpVBuffer = resource_new( 1024 * 1024 * sizeof( chik_vertex_t ) );
     if ( gpVBuffer == NULL ) {
         log_error( "Failed to create vertex resource.\n" );
         return INVALID_HANDLE;
@@ -421,9 +513,9 @@ void draw_vertex_buffer( handle_t sBuffer ) {
         /*
          *    If the triangle is facing away from the camera, skip it.
          */
-        if ( ma.v[ j + 8 ] > gpCamera->aNear && mb.v[ j + 8 ] > gpCamera->aNear && mc.v[ j + 8 ] > gpCamera->aNear ) {
+        /*if ( ma.v[ j + 8 ] > gpCamera->aNear && mb.v[ j + 8 ] > gpCamera->aNear && mc.v[ j + 8 ] > gpCamera->aNear ) {
             continue;
-        }
+        }*/
 
         chik_vertex_t a = a0;
         a.aPos = ( vec3_t ){ ma.v[ j ] / ma.v[ j + 12 ], ma.v[ j + 4 ] / ma.v[ j + 12 ], ma.v[ j + 8 ] / ma.v[ j + 12 ] };
@@ -434,7 +526,7 @@ void draw_vertex_buffer( handle_t sBuffer ) {
         chik_vertex_t c = c0;
         c.aPos = ( vec3_t ){ mc.v[ j ] / mc.v[ j + 12 ], mc.v[ j + 4 ] / mc.v[ j + 12 ], mc.v[ j + 8 ] / mc.v[ j + 12 ] };
 
-        draw_triangle( a, b, c );
+        draw_triangle( ( void* )&a, ( void* )&b, ( void* )&c );
     }
 }
 
